9236af0c74bc3dafce47e73421f1de999ae0bf30
Sofia Bernal <sb22i@fsu.edu>
2025-06-20 14:42:59 -0400

    updated home.tsx

--- app/home.tsx
+++ app/home.tsx
@@ -398,6 +398,7 @@ const styles = StyleSheet.create({
     textShadowColor: 'rgba(0, 0, 0, 0.3)',
     textShadowOffset: { width: 1, height: 1 },
     textShadowRadius: 3,
+    textAlign: 'center',
   },
   headerSubtitle: {
     fontSize: 16,
